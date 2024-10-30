5a2074bdf24c92845d7d511560bc3f02099e7cd1
Bruno Blazinc <odvratno.zgodan@gmail.com>
2024-10-30 10:37:02 +0100

    Add call logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-call-logging")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,6 +2,8 @@ package com.example
 
 import com.example.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.request.path
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -9,4 +11,9 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureRouting()
+    install(CallLogging) {
+        filter { call ->
+            !call.request.path().startsWith("/assets")
+        }
+    }
 }
